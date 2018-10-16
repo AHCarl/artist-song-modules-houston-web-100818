11ae56512a41ab72a39e440a25c08b5afc073a49
Alexander Carlin <ahcarlin@gmail.com>
2018-10-16 16:39:36 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -2,11 +2,11 @@ module Memorable
   require_relative '../artist.rb'
   require_relative '../song.rb'
   
-  self.reset_all
+  def self.reset_all
     self.all.clearn
   end
   
-  self.count
+  def self.count
     self.all.count
   end
   
